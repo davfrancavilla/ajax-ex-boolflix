d9c31e6ca80ae093b1d0963067707e0da97ffbce
Davide Francavilla <davfrancavilla@gmail.com>
2020-09-03 14:14:50 +0200

    Bandiere e stelle funzionanti

--- js/script.js
+++ js/script.js
@@ -3,10 +3,12 @@ $(document).ready(function(){
     var source = $("#template").html();
     var template = Handlebars.compile(source);
 
+    // ricerca dei film al click sul bottone nella pagina
     $("#search").click(function(){
         getMovies();
     });
 
+    // ricerca dei film alla pressione del tasto invio
     $("#movie-name").keydown(function (event) {
         if (event.which == 13 || event.keyCode == 13) {
             getMovies();
@@ -14,13 +16,14 @@ $(document).ready(function(){
     });
 
 
-    // functions
+    // FUNZIONI
 
+    // chiamata all'api che restuitisce film e relativi dettagli
     function getMovies() {
         $("#movies-list").empty();
         var movieName = $("#movie-name").val();
         if (movieName.length > 0) {
-            $("#query").val("");
+            $("#movie-name").val("");
             $.ajax({
                 url: "https://api.themoviedb.org/3/search/movie",
                 method: "GET",
@@ -31,20 +34,7 @@ $(document).ready(function(){
                     query: movieName
                 },
                 success: function (element) {
-                    if (element.results.length == 0){
-                        $("#movies-list").append("Nessun risultato");
-                    } else {
-                        for (var i = 0; i < element.results.length; i++) {
-                            var movie = {
-                                title: element.results[i].title,
-                                originalTitle: element.results[i].original_title,
-                                language: element.results[i].original_language,
-                                rating: element.results[i].vote_average
-                            }
-                            var html = template(movie);
-                            $("#movies-list").append(html);
-                        };
-                    }
+                    createMovies(element);
                 },
                 error: function (err) {
                     alert("Errore:" + err);
@@ -53,8 +43,51 @@ $(document).ready(function(){
         }
     }
 
+    // funzione che crea l'oggetto movie e lo appende alla lista dei film
+    function createMovies(movies){
+        if (movies.results.length == 0) { // caso in cui vengono restitui 0 film
+            $("#movies-list").append("Nessun risultato");
+        } else {
+            for (var i = 0; i < movies.results.length; i++) {
+                var movie = {
+                    title: movies.results[i].title,
+                    originalTitle: movies.results[i].original_title,
+                    language: setLanguage(movies.results[i].original_language),
+                    rating: setStars(movies.results[i].vote_average)
+                }
+                var html = template(movie);
+                $("#movies-list").append(html);
+            };
+        }
+        
+    };
 
+    // funziona che mostra il voto con 5 stelle invece che in numero
+    function setStars(apiRating){
+        var rating = Math.ceil(apiRating/2);
+        var fullStar = "<i class=\"fas fa-star\"></i>";
+        var emptyStar = "<i class=\"far fa-star\"></i>";
+        var vote = "";
+        for (var j = 0; j < rating; j++){
+            vote += fullStar;
+        }
+        for (var j = 0; j<5-rating; j++){
+            vote += emptyStar;
+        }
+        return vote
+    }
 
+    // funzione che sostituisce le lingue con le relative bandiere
+    function setLanguage(lang){
+        switch (lang) {
+            case "it":
+                return "<img src=\"img/en.png\" alt=\"italian-flag\">";
+            case "en":
+                return "<img src=\"img/it.png\" alt=\"english-flag\">";
+            default:
+                return lang;
+        }
+    };
 
 
 
