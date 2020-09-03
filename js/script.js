93af19dcd074727085015ad470e53727957bb0df
Davide Francavilla <davfrancavilla@gmail.com>
2020-09-03 15:12:14 +0200

    API per le serie TV aggiunta

--- js/script.js
+++ js/script.js
@@ -3,29 +3,35 @@ $(document).ready(function(){
     var source = $("#template").html();
     var template = Handlebars.compile(source);
 
+    var movieUrl = "https://api.themoviedb.org/3/search/movie";
+    var seriesUrl = "https://api.themoviedb.org/3/search/tv";
+
     // ricerca dei film al click sul bottone nella pagina
     $("#search").click(function(){
-        getMovies();
+        var movieName = $("#movie-name").val();
+        getMovies(movieUrl, "movies", movieName);
+        getMovies(seriesUrl, "series", movieName);
     });
 
     // ricerca dei film alla pressione del tasto invio
     $("#movie-name").keydown(function (event) {
         if (event.which == 13 || event.keyCode == 13) {
-            getMovies();
+            var movieName = $("#movie-name").val();
+            getMovies(movieUrl, "movies", movieName);
+            getMovies(seriesUrl, "series", movieName);
         }
     });
 
 
     // FUNZIONI
 
-    // chiamata all'api che restuitisce film e relativi dettagli
-    function getMovies() {
+    // chiamata all'api che restuitisce film o series tv (in base ai parametri passati) con i relativi dettagli
+    function getMovies(url, type, movieName) {
         $("#movies-list").empty();
-        var movieName = $("#movie-name").val();
         if (movieName.length > 0) {
             $("#movie-name").val("");
             $.ajax({
-                url: "https://api.themoviedb.org/3/search/movie",
+                url: url,
                 method: "GET",
                 data: {
                     api_key: "75a1a0a4142f1e4570128b8bfcc69bbf",
@@ -34,7 +40,7 @@ $(document).ready(function(){
                     query: movieName
                 },
                 success: function (element) {
-                    createMovies(element);
+                    createMovies(element, type);
                 },
                 error: function (err) {
                     alert("Errore:" + err);
@@ -44,38 +50,37 @@ $(document).ready(function(){
     }
 
     // funzione che crea l'oggetto movie e lo appende alla lista dei film
-    function createMovies(movies){
-        if (movies.results.length == 0) { // caso in cui vengono restitui 0 film
+    function createMovies(packet, type){
+        if (packet.results.length == 0) { // caso in cui vengono restitui 0 film
             $("#movies-list").append("Nessun risultato");
         } else {
-            for (var i = 0; i < movies.results.length; i++) {
-                var movie = {
-                    title: movies.results[i].title,
-                    originalTitle: movies.results[i].original_title,
-                    language: setLanguage(movies.results[i].original_language),
-                    rating: setStars(movies.results[i].vote_average)
-                }
-                var html = template(movie);
-                $("#movies-list").append(html);
+            if (type == "movies"){
+                for (var i = 0; i < packet.results.length; i++) {
+                    var movie = {
+                        type : "film",
+                        title: packet.results[i].title,
+                        originalTitle: packet.results[i].original_title,
+                        language: setLanguage(packet.results[i].original_language),
+                        rating: setStars(packet.results[i].vote_average)
+                    }
+                    var html = template(movie);
+                    $("#movies-list").append(html);
+                };
+            } else {
+                for (var i = 0; i < packet.results.length; i++) {
+                    var movie = {
+                        type : "series",
+                        title: packet.results[i].name,
+                        originalTitle: packet.results[i].original_name,
+                        language: setLanguage(packet.results[i].original_language),
+                        rating: setStars(packet.results[i].vote_average)
+                    }
+                    var html = template(movie);
+                    $("#movies-list").append(html);
+                };
             };
-        }
-        
+        };
     };
-
-    // funziona che mostra il voto con 5 stelle invece che in numero
-    function setStars(apiRating){
-        var rating = Math.ceil(apiRating/2);
-        var fullStar = "<i class=\"fas fa-star\"></i>";
-        var emptyStar = "<i class=\"far fa-star\"></i>";
-        var vote = "";
-        for (var j = 0; j < rating; j++){
-            vote += fullStar;
-        }
-        for (var j = 0; j<5-rating; j++){
-            vote += emptyStar;
-        }
-        return vote
-    }
 
     // funzione che sostituisce le lingue con le relative bandiere
     function setLanguage(lang){
@@ -88,6 +93,21 @@ $(document).ready(function(){
                 return lang;
         }
     };
+
+    // funziona che mostra il voto con 5 stelle invece che in numero
+    function setStars(apiRating) {
+        var rating = Math.ceil(apiRating / 2);
+        var fullStar = "<i class=\"fas fa-star\"></i>";
+        var emptyStar = "<i class=\"far fa-star\"></i>";
+        var vote = "";
+        for (var j = 0; j < rating; j++) {
+            vote += fullStar;
+        }
+        for (var j = 0; j < 5 - rating; j++) {
+            vote += emptyStar;
+        }
+        return vote
+    }
 
 
 
